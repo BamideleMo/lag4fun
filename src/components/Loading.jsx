1c291c152085ef1d5c4f6c3237dee3559bf487e1
bamidelemo <bamidelemo@gmail.com>
2024-05-18 14:42:52 +0100

    .

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,10 @@
 export default function Loading() {
   return (
     <>
-      <img src="/loading2.gif" class="w-14 my-20 mx-auto" />
+      {/* <img src="/loading2.gif" class="w-14 my-20 mx-auto" /> */}
+      <div class="text-gray-600 animate-pulse animate-bounce text-lg text-center">
+        Loading.. .
+      </div>
     </>
   );
 }
